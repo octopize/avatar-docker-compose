49059aa219890199d78a2743d981d9029b484a0c
Tom Crasset <25140344+tcrasset@users.noreply.github.com>
2022-11-18 10:18:35 +0100

    feat(helm): add pdfgenerator deployment (#12)
    * feat(helm): add pdfgenerator deployment
    
    * feat: add more resources to pdfgenerator pod

--- launch_helm.sh
+++ launch_helm.sh
@@ -13,7 +13,7 @@ if [ -z "$namespace" ] || [ -z "$release_name" ]; then
       exit 1
 fi
 
-if [ -z "$docker_pull_secret"]; then
+if [ -z "$docker_pull_secret" ]; then
       echo "You must supply DOCKER_PULL_SECRET."
       echo DOCKER_PULL_SECRET="$docker_pull_secret"
       exit 1
@@ -27,12 +27,14 @@ db_user="${DB_USER-avatar}"
 db_name="${DB_NAME-avatar}"
 
 avatar_version="${AVATAR_VERSION-latest}"
+pdfgenerator_version="${PDFGENERATOR_VERSION-latest}"
 use_email_auth="${USE_EMAIL_AUTH-false}"
 single_admin_email="${SINGLE_ADMIN_EMAIL-}" # We only accept a single email locally to not make the script overly complicated.
 aws_mail_account_access_key_id="${AWS_MAIL_ACCOUNT_ACCESS_KEY_ID-}"
 aws_mail_account_secret_access_key="${AWS_MAIL_ACCOUNT_SECRET_ACCESS_KEY-}"
 
 echo AVATAR_VERSION="$avatar_version"
+echo PDFGENERATOR_VERSION="$pdfgenerator_version"
 echo USE_EMAIL_AUTH="$use_email_auth"
 
 
@@ -60,10 +62,29 @@ else
 fi
 
 
-helm install "$release_name-redis" bitnami/redis --set auth.enabled=false --namespace "$namespace" --create-namespace 1> /dev/null
-helm install "$release_name-postgres" bitnami/postgresql --set auth.username="$db_user" --set auth.password="$db_password" --namespace "$namespace" --create-namespace 1> /dev/null
-
 postgres_release="$release_name-postgres"
+redis_release="$release_name-redis"
+
+only_avatar="${ONLY_AVATAR-}"
+echo ONLY_AVATAR="$only_avatar"
+if [ "$only_avatar" = "true" ]; then
+      echo "Creating only the avatar release. The API release will be launched but won't be accessible."
+else
+      if [ "$(helm list --namespace $namespace --filter $postgres_release -q)" = "$postgres_release" ]; then
+            echo "A postgres release already exists in that namespace. Set ONLY_AVATAR=true to only launch avatar."
+            exit 1
+      else
+            helm install "$postgres_release" bitnami/postgresql --set auth.username="$db_user" --set auth.password="$db_password" --namespace "$namespace" --create-namespace 1> /dev/null
+      fi
+
+      if [ "$(helm list --namespace $namespace --filter $redis_release -q)" = "$redis_release" ]; then
+            echo "A redis release already exists in that namespace. Set ONLY_AVATAR=true to only launch avatar."
+            exit 1
+      else
+            helm install "$redis_release" bitnami/redis --set auth.enabled=false --namespace "$namespace" --create-namespace 1> /dev/null
+      fi
+fi
+
 postgres_host="$postgres_release-postgresql.$namespace.svc.cluster.local"
 
 echo postgres_release="$postgres_release"
@@ -72,17 +93,30 @@ echo postgres_host="$postgres_host"
 redis_release="$release_name-redis"
 redis_host="$redis_release-master.$namespace.svc.cluster.local"
 
-timeout="${TIMEOUT-60}" # Time to wait for the pods to be up before exiting
-echo "Waiting for ${timeout} seconds for the pods to be up before continuing... You can configure the time using TIMEOUT"
-sleep ${timeout}
 
-# This is needed as passing --set auth.database=$db_name does not work. The database does not get created.
-echo "Create the database with 'create database ${db_name};' in the following psql prompt. Exit when done."
+if [ "$only_avatar" != "true" ]; then
+      timeout="${TIMEOUT-60}" # Time to wait for the pods to be up before exiting
+      echo "Waiting for ${timeout} seconds for the pods to be up before continuing... You can configure the time using TIMEOUT"
+      sleep ${timeout}
 
-kubectl run "$postgres_release-postgresql-client" --rm --tty -i --restart='Never' --namespace "$namespace" --image docker.io/bitnami/postgresql:14.5.0-debian-11-r6 --env="PGPASSWORD=$db_password" \
-      --command -- psql --host "$postgres_host" -U "$db_user" -d postgres -p 5432
+      # Use the password set during install instead of overring with new randomly generated one
+      pg_password="$(kubectl get secret --namespace $NAMESPACE $postgres_release-postgresql -o jsonpath="{.data.password}" | base64 -d)"
 
-cmd=(helm install --debug "$release_name" ./helm-chart --namespace "$namespace" --create-namespace \
+      # This is needed as passing --set auth.database=$db_name does not work. The database does not get created.
+      echo "Create the database with 'create database ${db_name};' in the following psql prompt. Exit when done."
+      kubectl run "$postgres_release-postgresql-client" --rm --tty -i --restart='Never' --namespace "$namespace" --image docker.io/bitnami/postgresql:14.5.0-debian-11-r6 --env="PGPASSWORD=$pg_password" \
+            --command -- psql --host "$postgres_host" -U "$db_user" -d postgres -p 5432
+fi
+
+# Use the password set during install instead of overring with new randomly generated one
+db_password="$(kubectl get secret --namespace $NAMESPACE $postgres_release-postgresql -o jsonpath="{.data.password}" | base64 -d)"
+
+upgrade="${UPGRADE-}"
+echo UPGRADE="$upgrade"
+subcommand=$([ "$upgrade" = "true" ] && echo "upgrade" || echo "install")
+
+cmd=(helm "$subcommand" --debug "$release_name" ./helm-chart --namespace "$namespace" --create-namespace \
+      --set api.baseUrl="http://api.octopize.local" \
       --set dockerPullSecret="$docker_pull_secret" \
       --set avatarVersion="$avatar_version" \
       --set dbPassword="$db_password" \
@@ -93,7 +127,8 @@ cmd=(helm install --debug "$release_name" ./helm-chart --namespace "$namespace" 
       --set api.pepper=$(python -c "import secrets; print(secrets.token_hex(), end='')") \
       --set api.authjwtSecretKey=$(python -c "import secrets; print(secrets.token_hex(), end='')") \
       --set api.fileEncryptionKey=$(python -c "import base64; import os; print(str(base64.urlsafe_b64encode(os.urandom(32)), encoding='utf-8'), end='')") \
-      --set api.avatarVersion="$avatar_version" \
+      --set avatarVersion="$avatar_version" \
+      --set pdfgeneratorVersion="$pdfgenerator_version" \
       --set api.useEmailAuthentication="$use_email_auth" \
 )
 

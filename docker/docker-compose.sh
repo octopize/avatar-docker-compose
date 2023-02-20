fe1cf219fd45e4eb9fbc1e5e095a1878944bf540
Tom Crasset <25140344+tcrasset@users.noreply.github.com>
2023-02-20 17:19:38 +0100

    feat(docker): create ready-to-install docker deployment (#16)

---
+++ docker/docker-compose.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This file is used to easily launch a default docker compose deployment.
+# Try to have default values for everything so that a simple `bash docker-compose.sh up`
+# launches the script
+
+args=()
+
+# If there is a docker-compose.local.yml file, use it to override the docker-compose configuration.
+if [[ -f "docker-compose.local.yml" ]]; then
+    args+=(-f docker-compose.local.yml)
+fi
+
+set -x
+
+
+export USER_ID=${USER_ID-$(id -u)}
+export SECRETS_DIR="${SECRETS_DIR:-.secrets}"
+export AVATAR_API_VERSION=${AVATAR_API_VERSION-latest}
+export AVATAR_PDFGENERATOR_VERSION=${AVATAR_PDFGENERATOR_VERSION-latest}
+export DOCKER_BUILDKIT=1
+export COMPOSE_PROJECT_NAME=avatar
+export HOST_SHARED_STORAGE_PATH="${HOST_SHARED_STORAGE_PATH-./shared}"
+
+docker compose --env-file=.env -f "docker-compose.yml" "${args[@]}" $@
